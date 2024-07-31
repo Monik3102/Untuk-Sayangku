6a87c62c72190c5f1b2f021fbffb8584d30146a2
Monik3102 <akunpaypalefraim7@gmail.com>
2024-07-31 16:09:45 +0700

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
   Swal.fire({
-    title: 'Mainkan Musik?',
+    title: 'Pake Musik Sayang ??',
     // text: "You won't be able to revert this!",
     icon: 'warning',
     showCancelButton: true,
